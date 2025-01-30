f20e419eeaf2d25f5b05d0fbcf4f5dedbffbf271
kuroroLuciferPhantom <91409971+kuroroLuciferPhantom@users.noreply.github.com>
2025-01-30 18:49:11 +0100

    Add App.js with routing configuration

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/home/HomePage';
+import LoginPage from './pages/auth/LoginPage';
+import OnboardingPage from './pages/onboarding/OnboardingPage';
+import GroupDetailPage from './pages/groups/GroupDetailPage';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/onboarding" element={<OnboardingPage />} />
+      <Route path="/group/:groupId" element={<GroupDetailPage />} />
+    </Routes>
+  );
+}
+
+export default App;
