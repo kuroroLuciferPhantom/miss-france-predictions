8efe10e11d4b5757dfe035a94a983ae3661a7c7e
kuroroLuciferPhantom <91409971+kuroroLuciferPhantom@users.noreply.github.com>
2025-01-30 20:20:33 +0100

    Update App.js to include SignUp route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/home/HomePage';
 import LoginPage from './pages/auth/LoginPage';
+import SignUpPage from './pages/auth/SignUpPage';
 import OnboardingPage from './pages/onboarding/OnboardingPage';
 import GroupDetailPage from './pages/groups/GroupDetailPage';
 
@@ -10,6 +11,7 @@ function App() {
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignUpPage />} />
       <Route path="/onboarding" element={<OnboardingPage />} />
       <Route path="/group/:groupId" element={<GroupDetailPage />} />
     </Routes>
