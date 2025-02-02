f456f4cd8c7081310b2fed72f912dd42db9c76bb
kuroroLuciferPhantom <91409971+kuroroLuciferPhantom@users.noreply.github.com>
2025-02-02 13:35:10 +0100

    feat: add missData.js with titles and candidates data

---
+++ src/data/missData.js
@@ -0,0 +1,52 @@
+// Liste des titres pour le top 5
+export const titles = [
+  "Miss France 2025",
+  "1ère Dauphine",
+  "2ème Dauphine",
+  "3ème Dauphine",
+  "4ème Dauphine"
+];
+
+// Liste des miss candidates
+export const missData = [
+  {
+    id: 1,
+    name: "Eve Gilles",
+    region: "Nord-Pas-de-Calais",
+    photo: "/assets/images/miss/nord-pas-de-calais.jpg",
+    socialMedia: {
+      instagram: "evegilles",
+      tiktok: "@evegilles"
+    }
+  },
+  {
+    id: 2,
+    name: "Adeline Vetter",
+    region: "Alsace",
+    photo: "/assets/images/miss/alsace.jpg",
+    socialMedia: {
+      instagram: "adelinevetter",
+      tiktok: "@adelinevetter"
+    }
+  },
+  {
+    id: 3,
+    name: "Marie Lambert",
+    region: "Bretagne",
+    photo: "/assets/images/miss/bretagne.jpg",
+    socialMedia: {
+      instagram: "marielambert",
+      tiktok: "@marielambert"
+    }
+  },
+  {
+    id: 4,
+    name: "Julie Petit",
+    region: "Provence",
+    photo: "/assets/images/miss/provence.jpg",
+    socialMedia: {
+      instagram: "juliepetit",
+      tiktok: "@juliepetit"
+    }
+  }
+];
